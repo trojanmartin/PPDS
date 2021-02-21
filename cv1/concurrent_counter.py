78d123ebbfb6258365516771b7fa1fe0d29dcea3
Martin <martin.trojan.372@gmail.com>
2021-02-21 20:00:02 +0100

    feat: added more version of counter method. Added docs.

--- cv1/concurrent_counter.py
+++ cv1/concurrent_counter.py
@@ -15,6 +15,44 @@ class Histogram(dict):
             self[item] = self.get(item, 0) + 1
 
 
+# Nespravne riesenie. Prakticky vzdy nastane uviaznutie.
+# Zámok sa zamkne,
+# avšak problém je že v prípade splnenej podmienky na riadku 25 cyklus skončí
+# dané vlákno sa ukončí
+# a zámok sa nikdy neodomkne.
+def counter_deadlock(shared):
+    while True:
+        shared.mutex.lock()
+        if shared.counter >= shared.end:
+            break
+
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
+
+
+# Nespravne riesenie.
+# Nastava OutOfIndex exception.
+# Zámok síce chráni manipuláciu s premmenými array a counter,
+# avšak môže nastať situácia
+# kedy je prvé vlákno je na riadku 47 až 48, a druhé vlákno je na riadku 44.
+# Podmienka nieje splnená.
+# Preto druhé vlákno pokračuje ďalej
+# medzitým však prvé vlákno zvýši honotu v premennej counter
+# a druhé vlákno sa dostane mimo rozsah poľa.
+def counter_out_of_index(shared):
+    while True:
+        if shared.counter >= shared.end:
+            break
+
+        shared.mutex.lock()
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
+
+
+# jedno zo "spravnych" rieseni. Zamok sa zamkne pristup k
+# zdielanemu objektu a odomkne sa aj v pripade ze podmienka nieje splnena
 def counter(shared):
     while True:
         shared.mutex.lock()
@@ -26,11 +64,13 @@ def counter(shared):
             shared.mutex.unlock()
             break
 
-for _ in range(1000):
-    sh = Shared(1_000_000)
-    t1 = Thread(counter, sh)
-    t2 = Thread(counter, sh)
 
+for _ in range(100):
+    sh = Shared(1_000_000)
+    t1 = Thread(counter_out_of_index, sh)
+    t2 = Thread(counter_out_of_index, sh)
+
+    # čakanie na ukončenie činnosti vlákien.
     t1.join()
     t2.join()
 
