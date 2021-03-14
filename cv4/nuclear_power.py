ffec49d8318182311d8afb4cd1554d640d297036
Martin <martin.trojan.372@gmail.com>
2021-03-14 19:50:52 +0100

    feat: implement method monitor and cidlo

--- cv4/nuclear_power.py
+++ cv4/nuclear_power.py
@@ -1,4 +1,4 @@
-from fei.ppds import Thread, Semaphore, print, Mutex
+from fei.ppds import Thread, Semaphore, print, Mutex, Event
 from time import sleep
 from random import randint
 
@@ -10,10 +10,12 @@ class LightSwitch():
 
     def lock(self, semaphore):
         self.mutex.lock()
+        count = self.counter
         self.counter += 1
         if self.counter == 1:
             semaphore.wait()
         self.mutex.unlock()
+        return count
 
     def unlock(self, semaphore):
         self.mutex.lock()
@@ -21,3 +23,52 @@ class LightSwitch():
         if self.counter == 0:
             semaphore.signal()
         self.mutex.unlock()
+
+
+class Elektraren():
+    def __init__(self):
+        self.cidla_ls = LightSwitch()        
+        self.monitor_ls = LightSwitch()
+        self.no_cidla = Semaphore(1)
+        self.no_monitor = Semaphore(1)       
+
+    def monitor(self,monitor_id):
+        while True:                     
+            self.no_monitor.wait()                       
+
+            pocet_citajucich_monitorov = self.monitor_ls.lock(self.no_cidla)
+            self.no_monitor.signal()
+            waiting = randint(40,50)/1000
+            print('monit "%02d": pocet_citajucich_monitorov=%02d, trvanie_citania=%03f\n' % (monitor_id,pocet_citajucich_monitorov,waiting))
+            sleep(waiting)            
+            self.monitor_ls.unlock(self.no_cidla)
+
+
+    def cidlo(self, cidlo_id, waiting):
+        while True:
+            sleep(randint(50,60)/1000)
+            
+            pocet_zapisujucich_cidiel = self.cidla_ls.lock(self.no_monitor)
+            self.no_cidla.wait()
+
+            print('cidlo "%02d": pocet_zapisujucich_cidiel=%02d, trvanie_zapisu=%03f\n' % (cidlo_id,pocet_zapisujucich_cidiel,waiting))
+            sleep(waiting)  
+            self.no_cidla.signal()     
+            self.cidla_ls.unlock(self.no_monitor)
+            
+
+elek = Elektraren()
+threads = []
+for i in range(2):
+    t = Thread(elek.cidlo, i,randint(10,20)/1000)
+    threads.append(t)
+
+t = Thread(elek.cidlo, 2, randint(20,25)/1000)
+threads.append(t)
+
+for i in range(8):
+    t = Thread(elek.monitor, i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
