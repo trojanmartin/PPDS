99b0ddd4138ed3f56df147c1220b4876859223bb
Martin <martin.trojan.372@gmail.com>
2021-03-21 16:05:20 +0100

    feat: implementing cooking

--- cv5/savage_and_cook.py
+++ cv5/savage_and_cook.py
@@ -1,4 +1,4 @@
-from fei.ppds import Semaphore, Mutex, Thread, print
+from fei.ppds import Semaphore, Mutex, Thread,Event, print
 from random import randint
 from time import sleep
  
@@ -6,9 +6,11 @@ from time import sleep
 Preto ich nedavame do zdielaneho objektu.
     M - pocet porcii misionara, ktore sa zmestia do hrnca.
     N - pocet divochov v kmeni (kuchara nepocitame).
+    K - pocet kucharov
 """
-M = 2
+M = 5
 N = 3
+K = 3
  
  
 class SimpleBarrier:
@@ -40,22 +42,14 @@ class SimpleBarrier:
         self.sem.wait()
  
  
-class Shared:
-    """V tomto pripade musime pouzit zdielanu strukturu.
-    Kedze Python struktury nema, pouzijeme triedu bez vlastnych metod.
-    Preco musime pouzit strukturu? Lebo chceme zdielat hodnotu
-    pocitadla servings, a to jednoduchsie v Pythone asi neurobime.
-    Okrem toho je rozumne mat vsetky synchronizacne objekty spolu.
-    Pri zmene nemusime upravovat API kazdej funkcie zvlast.
-    """
- 
+class Shared: 
     def __init__(self):
         self.mutex = Mutex()
+        self.cook_mutex = Mutex()
+        self.signaled = False
         self.servings = 0
         self.full_pot = Semaphore(0)
-        self.empty_pot = Semaphore(0)
-        self.barrier1 = SimpleBarrier(N)
-        self.barrier2 = SimpleBarrier(N)
+        self.empty_pot = Event()
  
  
 def get_serving_from_pot(savage_id, shared):
@@ -75,48 +69,37 @@ def eat(savage_id):
  
  
 def savage(savage_id, shared):
-    while True:
-        """Pred kazdou hostinou sa divosi musia pockat.
-        Kedze mame kod vlakna (divocha) v cykle, musime pouzit dve
-        jednoduche bariery za sebou alebo jednu zlozenu, ale kvoli
-        prehladnosti vypisov sme sa rozhodli pre toto riesenie.
-        """
- 
-        shared.barrier1.wait(
-            "divoch %2d: prisiel som na veceru, uz nas je %2d",
-            savage_id,
-            print_each_thread=True)
-        shared.barrier2.wait("divoch %2d: uz sme vsetci, zaciname vecerat",
-                             savage_id,
-                             print_last_thread=True)
- 
+    while True: 
         # Nasleduje klasicke riesenie problemu hodujucich divochov.
         shared.mutex.lock()
         print("divoch %2d: pocet zostavajucich porcii v hrnci je %2d" %
               (savage_id, shared.servings))
         if shared.servings == 0:
             print("divoch %2d: budim kuchara" % savage_id)
-            shared.empty_pot.signal()
+            shared.empty_pot.set()
             shared.full_pot.wait()
         get_serving_from_pot(savage_id, shared)
         shared.mutex.unlock()
  
         eat(savage_id)
  
+
+def cooking(cook_id):
+      # navarenie jedla tiez cosi trva...
+    print("kuchar %2d vari" % cook_id)
+    sleep(0.4 + randint(0, 2) / 10)
+
  
-def put_servings_in_pot(M, shared):
+def put_servings_in_pot(cook_id, shared):
     """M je pocet porcii, ktore vklada kuchar do hrnca.
     Hrniec je reprezentovany zdielanou premennou servings.
     Ta udrziava informaciu o tom, kolko porcii je v hrnci k dispozicii.
     """
- 
-    print("kuchar: varim")
-    # navarenie jedla tiez cosi trva...
-    sleep(0.4 + randint(0, 2) / 10)
-    shared.servings += M
+    print("kuchar %2d vklada missionara do hrnca" % cook_id)
+    shared.servings += 1
  
  
-def cook(M, shared):
+def cook(cook_id, M, shared):
     """Na strane kuchara netreba robit ziadne modifikacie kodu.
     Riesenie je standardne podla prednasky.
     Navyse je iba argument M, ktorym explicitne hovorime, kolko porcii
@@ -126,9 +109,28 @@ def cook(M, shared):
     """
  
     while True:
+
+        """Vsetci kuchari cakaju kym bude hrniec prazdny.
+        Ked sa hrniec vyprazdni zacnu vsetci varit.
+        """
         shared.empty_pot.wait()
-        put_servings_in_pot(M, shared)
-        shared.full_pot.signal()
+        cooking(cook_id)
+
+        """Ak uz je hrnciec plny, a zaroven event o prazdnom hrnci
+        nieje nastaveny, dam signal divochom ze mozu hodovat
+        Ak je event nastaveny a vlakno sa nachadza v mutexe, znamena
+        to ze niekto pred nim uz oznamil divochom radostnu spravu o plnom hrnci
+        a dane vlakno nemusi robit nic"""
+        shared.cook_mutex.lock()               
+        if(M == shared.servings and shared.empty_pot.isSet()):
+            shared.empty_pot.clear()
+            shared.full_pot.signal()
+        shared.cook_mutex.unlock()
+
+        """Ak som navaril, ale medzitym sa hrniec naplnil cakam
+        kym bude znova prazdny"""
+        shared.empty_pot.wait()
+        put_servings_in_pot(cook_id, shared)        
  
  
 def init_and_run(N, M):
@@ -137,7 +139,9 @@ def init_and_run(N, M):
     shared = Shared()
     for savage_id in range(0, N):
         threads.append(Thread(savage, savage_id, shared))
-    threads.append(Thread(cook, M, shared))
+    
+    for cook_id in range(0, K):
+        threads.append(Thread(cook, cook_id, M, shared))
  
     for t in threads:
         t.join()
