0b7e62a62f7b9342a237e0533a177aaf2a293320
Martin <martin.trojan.372@gmail.com>
2021-03-28 17:58:08 +0200

    feat: implement oxygen method

--- cv6/h2o.py
+++ cv6/h2o.py
@@ -1,3 +1,54 @@
 from fei.ppds import Semaphore, Mutex, Thread, Event, print
 from random import randint
 from time import sleep
+
+
+class Barrier:
+    def __init__(self, N):
+        self.N = N
+        self.counter = 0
+        self.mutex = Mutex()
+        self.turnstile = Semaphore(0)
+ 
+    def wait(self):
+        self.mutex.lock()
+        self.counter += 1
+    
+        if self.counter == self.N:
+            self.turnstile.signal()
+        self.mutex.unlock()
+         
+        self.turnstile.wait()
+        self.turnstile.signal()
+
+
+class Shared():
+    def __init__(self):
+        self.mutex = Semaphore(1)
+        self.hydrogen = 0
+        self.oxygen = 0      
+        self.barrier = Barrier(3)
+        self.oxygenQueue = Semaphore(0)
+        self.hydrogenQueue = Semaphore(0)  
+
+def bond():
+    pass
+
+def oxygen(shared):
+    shared.mutex.wait()
+    shared.oxygen += 1
+
+    if(shared.hydrogen < 2):
+        shared.mutex.signal()
+
+    else:
+        shared.oxygen -= 1
+        shared.hydrogen -= 2
+        shared.oxygenQueue.signal()
+        shared.hydrogenQueue.signal(2)
+
+    shared.oxygenQueue.wait()
+    bond()
+
+    shared.barrier.wait()
+    shared.mutex.signal()
