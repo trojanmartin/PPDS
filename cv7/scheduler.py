52796c1a132df4112fea41c1e990eca2ef82074c
Martin <martin.trojan.372@gmail.com>
2021-04-12 20:45:14 +0200

    feat: add class coroutine and scheduler

--- cv7/scheduler.py
+++ cv7/scheduler.py
@@ -1,3 +1,6 @@
+import queue
+from enum import Enum
+
 def test1():
     while True:
         print "Test 1"
@@ -8,3 +11,32 @@ def test2():
     while True:
         print "Test 2"
         yield
+
+
+class Coroutine:
+    def __init__(self, method):
+        self.method = method
+        self.finished = False
+
+    def run(self):
+        try:
+            self.method.send()
+        except GeneratorExit:
+            self.finished = True
+
+
+class CoroutineScheduler:
+    def __init__(self):
+        self.couroutines = PriorityQueue()
+
+    def add(self, couroutine, priority):
+        self.couroutines.put((priority, couroutine))     
+    
+
+    def run(self):
+        while True:
+            self.couroutines.pop()
+
+
+
+    
