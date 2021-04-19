04acadda0b6578d71bedf386a7b76c1181f156cc
Martin <martin.trojan.372@gmail.com>
2021-04-19 20:00:04 +0200

    feat: add sync version of cv8

---
+++ cv8/sync.py
@@ -0,0 +1,24 @@
+import requests
+
+class Shared:
+    def __init__(self):        
+        self.finished = False
+
+def Progress(shared):
+    while(not shared.finished):
+        sleep(0.01)
+        print("In progress")
+
+
+def download(shared):
+    print("Starting dowloading")
+    r = requests.get(url = "https://www.google.com")
+    shared.finished = True    
+    print("Downloading finished")        
+
+def main():
+    download()        
+
+
+if __name__ == "__main__":
+    main()
