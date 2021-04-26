48123f7e9878fe593e0309db3d376f4fd55afcdb
Martin <martin.trojan.372@gmail.com>
2021-04-26 21:47:28 +0200

    feat: add second array

--- cv9/cuda.py
+++ cv9/cuda.py
@@ -6,15 +6,16 @@ import math
 
 # CUDA kernel
 @cuda.jit
-def my_kernel(io_array):
+def my_kernel(first_array, second_array):
     pos = cuda.grid(1)
-    if pos < io_array.size:
-        io_array[pos] *= 2 # do the computation
+    if pos < first_array.size:
+        first_array[pos] += second_array[pos] # do the computation
 
 
 # Host code   
-data = numpy.ones(256)
+first_array = numpy.ones(256)
+second_array = numpy.ones(256) * 10
 threadsperblock = 256
-blockspergrid = math.ceil(data.shape[0] / threadsperblock)
-my_kernel[blockspergrid, threadsperblock](data)
-print(data)
+blockspergrid = math.ceil(first_array.shape[0] / threadsperblock)
+my_kernel[blockspergrid, threadsperblock](first_array, second_array)
+print(first_array)
